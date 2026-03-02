0a9611deeca59f18b04713a2340508460ddd1556
Mr Wizard <alex@pshul.com>
2026-03-02 18:59:06 -0500

    Converted the boilerplate endpoint to the cms/events/ and made it echo for testing the simulator

--- CmsObserver/CmsObserver.API/Program.cs
+++ CmsObserver/CmsObserver.API/Program.cs
@@ -1,12 +1,11 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +13,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapPost("/cms/events", async (HttpRequest request) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    using var document = await JsonDocument.ParseAsync(request.Body);
+    var events = document.RootElement.Clone();
+    Console.WriteLine("Received: ");
+    Console.WriteLine(document.RootElement.ToString());
+    return Results.Ok(events);
+});
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
